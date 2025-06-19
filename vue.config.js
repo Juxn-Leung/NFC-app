babdfe12c29070134c717fc75f904a55c31b0a57
junxuan.liang@fftai.com <koudai0000>
2025-06-20 00:54:16 +0800

    perf：云函数调整

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
     plugins: [
       new CopyWebpackPlugin([
         {
-          from: path.join(__dirname, 'functions'),
-          to: path.join(__dirname, 'unpackage', process.env.NODE_ENV === 'production' ? 'build' : 'dev', process.env.UNI_PLATFORM, 'functions')
+          from: path.join(__dirname, 'cloudfunctionTemplate'),
+          to: path.join(__dirname, 'unpackage/dist', process.env.NODE_ENV === 'production' ? 'build' : 'dev', process.env.UNI_PLATFORM, 'cloudfunctionTemplate')
         }
       ])
     ]
