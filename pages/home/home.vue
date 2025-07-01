b5aad046ebd5a9375dc6c30147a1464718af894d
Juno <1466098865@qq.com>
2025-07-01 19:46:55 +0800

    perf：增加幾張圖片

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -37,6 +37,14 @@
           <text class="text">编辑内容</text>
         </view>
       </view>
+
+      <u-image
+        class="popup-background"
+        width="100%"
+        height="100%"
+        src="@/static/images/background.png"
+        mode="aspectFill"
+      ></u-image>
     </view>
 
     <u-popup
@@ -419,44 +427,6 @@ export default {
     background: transparent;
     position: relative;
   }
-}
-
-.home {
-  font-family: 'Quicksand', sans-serif;
-  margin: 0;
-  min-height: 100vh;
-  background-color: #e493d0;
-  background-image: radial-gradient(closest-side,
-      rgba(235, 105, 78, 1),
-      rgba(235, 105, 78, 0)),
-    radial-gradient(closest-side, rgba(243, 11, 164, 1), rgba(243, 11, 164, 0)),
-    radial-gradient(closest-side,
-      rgba(254, 234, 131, 1),
-      rgba(254, 234, 131, 0)),
-    radial-gradient(closest-side,
-      rgba(170, 142, 245, 1),
-      rgba(170, 142, 245, 0)),
-    radial-gradient(closest-side,
-      rgba(248, 192, 147, 1),
-      rgba(248, 192, 147, 0));
-  background-size: 130vmax 130vmax, 80vmax 80vmax, 90vmax 90vmax,
-    110vmax 110vmax, 90vmax 90vmax;
-  background-position: -80vmax -80vmax, 60vmax -30vmax, 10vmax 10vmax,
-    -30vmax -10vmax, 50vmax 50vmax;
-  background-repeat: no-repeat;
-  animation: 10s movement linear infinite;
-}
-
-.home::after {
-  content: '';
-  display: block;
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  backdrop-filter: blur(10px);
-  -webkit-backdrop-filter: blur(10px);
 }
 
 span {
